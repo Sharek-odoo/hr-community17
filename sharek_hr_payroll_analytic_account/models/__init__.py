38b3b17c7b627f18ac52e3c4506f7b0259f4a77b
Malaz-svg <malazmohammed29@gmail.com>
2026-01-14 21:53:27 +0200

    update sharek_hr_payroll_analytic_account

--- sharek_hr_payroll_analytic_account/models/__init__.py
+++ sharek_hr_payroll_analytic_account/models/__init__.py
@@ -5,6 +5,6 @@
 #    Copyright (C) 2024 Sharek Telecom & IT Redefined(https://sharek.com.sa).
 #    Author: Sharek Telecom & IT Redefined
 from . import account_type
-from . import hr_payroll_account
+# from . import hr_payroll_account
 from . import res_company
 from . import res_config_settings
